4be7eee16d96169fa96279a8d1ded480820fd828
AmrAlsyed <Xaxeth@gmail.com>
2025-11-03 04:42:44 +0200

    added prod server

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+const getBaseURL = () => {
+  if (process.env.NODE_ENV === "production") {
+    return "https://todo.twfyer.com";
+  }
+  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
+};
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000",
+  baseURL: getBaseURL(),
   headers: { "Content-Type": "application/json" },
 });
